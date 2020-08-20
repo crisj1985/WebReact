13f5548d00895663aa8d20171172ee8dac15974a
crisj1985 <crisj1985@hotmail.com>
2020-08-20 00:33:55 -0500

    Mobile  First -  font

--- src/Components/Nav/RigthNav.jsx
+++ src/Components/Nav/RigthNav.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Ul = styled.ul`
- 
-    list-style: none;
-    display: flex;
-    flex-flow: column nowrap;
+  list-style: none;
+  display: flex;
+  flex-flow: column nowrap;
+  background-color:#BBC7D6;
+  position: fixed;
+  top:0;
+  right:0;
+  height:100vh;
+  width:300px;
+  padding-top:2rem;
 
-    li {
-      padding: 18px 10px;
-    }
-  
+   li {
+    padding: 20px 8px;
+  }
 `;
 
 const RigthNav = () => {
